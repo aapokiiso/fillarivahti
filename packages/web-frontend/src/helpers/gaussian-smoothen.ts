d2be9de6b65c200d9d21f66ea3da20fbc9d717dc
Aapo Kiiso <aapo@lamia.fi>
2021-09-02 18:52:48 +0300

    Improve gaussian smoother comments

--- packages/web-frontend/src/helpers/gaussian-smoothen.ts
+++ packages/web-frontend/src/helpers/gaussian-smoothen.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-magic-numbers */
+
 /**
  * Sums numbers in dataset.
  *
@@ -8,10 +10,12 @@ const sum = (dataset: number[]): number =>
     dataset.reduce((sum, value) => sum + value, 0);
 
 /**
- * Kernel radius of 2 is a sensible default for most use cases. It smoothens
- * any entry in the dataset with its 2 closest neighbors on both sides.
+ * Standard deviation (sigma) of 2 makes the smoothing take into account two
+ * adjacent values on both sides of a value when smoothing. It it a good
+ * default for most time-series data, since it smoothens the biggest deviations
+ * while keeping the overall trend visible.
  */
-const DEFAULT_KERNEL_RADIUS = 2;
+const DEFAULT_SIGMA = 2;
 
 /**
  * Applies Gaussian smoothing on the dataset.
@@ -19,33 +23,29 @@ const DEFAULT_KERNEL_RADIUS = 2;
  * @see https://en.wikipedia.org/wiki/Kernel_smoother#Gaussian_kernel_smoother
  *
  * @param {Number[]} dataset
- * @param {Number} [kernelRadius]
+ * @param {Number} [sigma]
  * @returns {Number[]}
  *
  * @throws {Error} If dataset is too small
  */
-const gaussianSmoothen = (dataset: number[], kernelRadius: number = DEFAULT_KERNEL_RADIUS): number[] => {
-    const radiusFactor = 2;
-    const kernelDiameter = radiusFactor * kernelRadius + 1;
+const gaussianSmoothen = (dataset: number[], sigma: number = DEFAULT_SIGMA): number[] => {
+    const kernelWidth = 2 * sigma + 1;
 
-    if (dataset.length < kernelDiameter) {
-        throw new Error(`Dataset needs to be larger than the kernel diameter (${kernelDiameter}).`);
+    if (dataset.length < kernelWidth) {
+        throw new Error(`Dataset needs to be larger than the kernel diameter (${kernelWidth}).`);
     }
 
     return dataset.map((value, idx1) => {
         // Dataset edges would need circular processing.
-        if (idx1 - kernelRadius < 0 || idx1 + kernelRadius >= dataset.length) {
+        if (idx1 - sigma < 0 || idx1 + sigma >= dataset.length) {
             return value;
         }
-
-        const squareExponent = 2;
-        const kernelFactor = 2;
 
         const kernel = dataset.map(
             (_, idx2) => Math.exp(
                 -1 * (
-                    Math.pow(idx2 - idx1, squareExponent)
-                    / (kernelFactor * Math.pow(kernelRadius, squareExponent))
+                    Math.pow(idx2 - idx1, 2)
+                    / (2 * Math.pow(sigma, 2))
                 ),
             ),
         );
