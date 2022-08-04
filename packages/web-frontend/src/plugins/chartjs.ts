f32837b9e0c8e6b84ce24f8911c6815636c7a339
Aapo Kiiso <aapo@lamia.fi>
2022-08-05 00:51:32 +0300

    Remove old Nuxt 2 frontend

--- packages/web-frontend/src/plugins/chartjs.ts
+++
@@ -1,2 +0,0 @@
-import { Chart, LineElement, PointElement, LineController, CategoryScale, LinearScale, Filler, Legend } from 'chart.js';
-Chart.register(LineElement, PointElement, LineController, CategoryScale, LinearScale, Filler, Legend);
--- packages/web-frontend/src/plugins/loading-skeleton.ts
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import PuSkeleton from 'vue-loading-skeleton';
-
-Vue.use(PuSkeleton);
