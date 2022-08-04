3798855b2c8a86c3c298e023552c0d93dd92da08
Aapo Kiiso <aapo@lamia.fi>
2022-08-04 22:51:14 +0300

    Add web app manifest to Nuxt 3 frontend

--- packages/web-frontend-nuxt3/app.vue
+++ packages/web-frontend-nuxt3/app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <Html class="h-full bg-gray-100" />
+
+    <Head>
+      <Title>{{ $t('meta.title') }}</Title>
+      <Meta name="description" :content="$t('meta.description')" />
+      <Meta name="charset" content="utf-8" />
+      <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Link rel="manifest" href="manifest.json" />
+    </Head>
+
     <Body class="h-full" />
 
     <!-- Toggleable sidebar for mobile -->
